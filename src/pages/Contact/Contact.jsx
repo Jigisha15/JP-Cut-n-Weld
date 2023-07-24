f79cb8cf3cf0e849e60ec25f7fc5ed13eac565a1
Jigisha Manohar <jigishamanohar18@gmail.com>
2023-07-25 02:21:08 +0530

    Contact Page UI and working

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,6 @@
 import React, { useRef } from 'react'
 import emailjs from '@emailjs/browser'
 
-import bgi from '../../assets/contact-img.jpg'
-
 import './Contact.css'
 
 const Contact = () => {
@@ -28,88 +26,64 @@ const Contact = () => {
     e.target.reset()
   }
 
-  // return (
-  //   <div className="big-container">
-  //     <div className="image">
-  //       <img
-  //         src={bgi}
-  //         alt=""
-  //         className="contact-image"
-  //       />
-  //     </div>
-  //     <div className="contact">
-  //       <h1>Contact Us!</h1>
-  //       <form
-  //         ref={form}
-  //         onSubmit={sendEmail}
-  //         action=""
-  //         method="post"
-  //       >
-  //         <div className="form-class">
-  //           <input
-  //             type="text"
-  //             name="name"
-  //           />
-  //           <label htmlFor="name">Name</label>
-  //         </div>
-  //         <br />
-  //         <div className="form-class">
-  //           <input
-  //             type="tel"
-  //             name="phone"
-  //           />
-  //           <label htmlFor="phone">Contact number</label>
-  //         </div>
-
-  //         <br />
-  //         <div className="form-class">
-  //           <input
-  //             type="email"
-  //             name="email"
-  //           />
-  //           <label htmlFor="email">Email ID</label>
-  //         </div>
-
-  //         <br />
-  //         <div className="form-class">
-  //           <textarea
-  //             rows={5}
-  //             type="text"
-  //             name="feedback"
-  //           />
-  //           <label htmlFor="feedback">Message</label>
-  //         </div>
-
-  //         <br />
-  //         <button
-  //           type="submit"
-  //           value="Submit"
-  //         >
-  //           Submit
-  //         </button>
-  //         {/* <input
-  //         type="submit"
-  //         placeholder="Submit"
-  //         style={{ padding: '5px 7px', fontSize: '15px', cursor: 'pointer' }}
-  //       /> */}
-  //       </form>
-  //     </div>
-  //   </div>
-  // )
-
   return (
-    <div className="contact">
-      <div className="c-image">
-        <img
-          className="cim"
-          src={bgi}
-          alt=""
-        />
+    <>
+      <h1 className="contact-title">Contact Us!</h1>
+      <div className="contact">
+        <div className="c-image"></div>
+        <div className="contact-form">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            action=""
+            method="post"
+          >
+            <div className="form-class">
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                required
+              />
+            </div>
+            <div className="form-class">
+              <input
+                type="tel"
+                name="phone"
+                placeholder="Contact Number"
+                required
+              />
+            </div>
+
+            <div className="form-class">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email ID"
+                required
+              />
+            </div>
+
+            <div className="form-class">
+              <textarea
+                rows={5}
+                type="text"
+                name="feedback"
+                placeholder="Message / Feedback"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              value="Submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
-      <div className="contact-form">
-        <h1>Hi</h1>
-      </div>
-    </div>
+    </>
   )
 }
 
