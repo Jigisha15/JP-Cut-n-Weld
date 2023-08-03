e6abcff0e82750500859a53e1af11ea869bc2f82
Jigisha Manohar <jigishamanohar18@gmail.com>
2023-08-04 04:28:12 +0530

    navbar, footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,44 +1,105 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { BiCopyright } from 'react-icons/bi'
 import { HiPhone } from 'react-icons/hi'
+import { LiaMapSolid } from 'react-icons/lia'
 import { BiLogoGmail } from 'react-icons/bi'
+import { IoLogoWhatsapp } from 'react-icons/io5'
 
 import './Footer.css'
 
 const Footer = () => {
+  const handleClick = () => {
+    alert("Thanks! We'll keep you updated!")
+  }
+  const handlePhone = () => {
+    alert('Copy this number manually : 8108238874')
+  }
+
   return (
+    // <div className="footer">
+    //   <div className="footer-main">
+    //     <div className="left foot">
+    //       <p>
+    //         We're in this industry since a decade. We provide the best machines
+    //         and a good serrvice alogn with a guaranteed maintainence.
+    //       </p>
+    //     </div>
+    //     <div className="middle foot">
+    //       <h1>Quick Contacts</h1>
+    //       <span>
+    //         <HiPhone /> : 123456789
+    //       </span>
+    //       <span>
+    //         <BiLogoGmail /> : jpcnc@gmail.com
+    //       </span>
+    //     </div>
+    //     <div className="right foot">
+    //       <h1>Locate Us!</h1>
+    //       <iframe
+    //         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.0018856158354!2d72.84816747495645!3d19.15139474960176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b64a9f3b67a9%3A0x28608ca94599266a!2sRam%20Mandir!5e0!3m2!1sen!2sin!4v1689864469859!5m2!1sen!2sin"
+    //         width={200}
+    //         height={150}
+    //         style={{ border: 0 }}
+    //         allowfullscreen=""
+    //         loading="lazy"
+    //         referrerpolicy="no-referrer-when-downgrade"
+    //       ></iframe>
+    //     </div>
+    //   </div>
+    //   <div className="dev">
+    //     Developed by :{' '}
+    //     <Link
+    //       className="dev-name"
+    //       to="https://portfolio-jigisha15.vercel.app/"
+    //       target="_blank"
+    //     >
+    //       Jigisha Ghanekar
+    //     </Link>
+    //   </div>
+    // </div>
+
     <div className="footer">
-      <div className="footer-main">
-        <div className="left foot">
+      <div className="footer-texts">
+        <div className="left-f-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          nobis dolor commodi temporibus quo, ea et consequuntur quis ad non
+          doloribus, optio recusandae dolore ipsa sequi suscipit in voluptates
+          laudantium necessitatibus cumque nulla! Assumenda atque error, ratione
+          aut reprehenderit deleniti!
+        </div>
+        <div className="middle-f-text">
+          <h1>Want to recieve Notification Email from us to stay updated?</h1>
+          <button onClick={handleClick}>YES</button>
+        </div>
+        <div className="right-f-text">
+          <h1>Office Address</h1>
           <p>
-            We're in this industry since a decade. We provide the best machines
-            and a good serrvice alogn with a guaranteed maintainence.
+            Business Complex, Nr Petrol Pump, IB Patel Road, Goregaon(E),
+            Mumbai-400063
           </p>
-        </div>
-        <div className="middle foot">
-          <h1>Quick Contacts</h1>
-          <span>
-            <HiPhone /> : 123456789
+          <span className="f-span">
+            {/* <button
+              value="8108238874"
+              onClick={handlePhone}
+            > */}
+            <HiPhone
+              color="#b1d0fe"
+              size={14}
+              value="8108238874"
+            />
+            {/* </button> */}
+            <IoLogoWhatsapp color="#25D366" />
+            <BiLogoGmail color="blue" />
+            <LiaMapSolid color="red" />
           </span>
-          <span>
-            <BiLogoGmail /> : jpcnc@gmail.com
-          </span>
-        </div>
-        <div className="right foot">
-          <h1>Locate Us!</h1>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.0018856158354!2d72.84816747495645!3d19.15139474960176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b64a9f3b67a9%3A0x28608ca94599266a!2sRam%20Mandir!5e0!3m2!1sen!2sin!4v1689864469859!5m2!1sen!2sin"
-            width={200}
-            height={150}
-            style={{ border: 0 }}
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
         </div>
       </div>
-      <div className="dev">
+      <div className="copyright">
+        <BiCopyright /> Copyrights Reserved
+      </div>
+      <div className="developer">
         Developed by :{' '}
         <Link
           className="dev-name"
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -10,104 +10,112 @@ import { TfiAngleLeft } from 'react-icons/tfi'
 
 import './LandingPage.css'
 
+// const LandingPage = () => {
+//   const carousel = [
+//     {
+//       id: 1,
+//       image: m1,
+//     },
+//     {
+//       id: 2,
+//       image: m2,
+//     },
+//     {
+//       id: 3,
+//       image: m3,
+//     },
+//   ]
+
+//   console.log(carousel)
+
+//   const length = carousel.length
+//   const [click, setClick] = useState(0)
+
+//   const prevClick = () => {
+//     setClick(click === length - 1 ? 0 : click + 1)
+//   }
+
+//   const nextClick = () => {
+//     setClick(click === 0 ? length - 1 : click - 1)
+//   }
+
+//   const [num, setNum] = useState(0)
+//   useEffect(() => {
+//     if (num < 250) {
+//       setNum((num) => num + 1)
+//     } else {
+//       setNum(num)
+//     }
+//   })
+
+//   return (
+//     <div className="landing-page">
+//       <div className="landing-top">
+//         <div className="intro">
+//           <span className="s1">
+//             Experience the best services from the most trusted
+//           </span>
+//           <span className="s2">JP Cut & Weld</span>
+//         </div>
+//         <div className="nums">
+//           <div className="num1 num">
+//             <p>Our count of happy customers is increasing everyday!</p>
+//             <h1>{num}+</h1>
+//           </div>
+//           <div className="num2 num">
+//             <p>Number of places we have provided services...</p>
+//             <h1>{num}+</h1>
+//           </div>
+//           <div className="num3 num">
+//             <p>
+//               We strive to be better than yesterday and give the best than we
+//               gave yesterday.
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="landing-middle">
+//         <div className="landing-left">
+//           <button onClick={prevClick}>
+//             <TfiAngleLeft />
+//           </button>
+//           {carousel.map((item, index) => (
+//             <div
+//               className={index === click ? 'carousel active' : 'carousel'}
+//               key={index}
+//             >
+//               {index === click && (
+//                 <img
+//                   className="car-image"
+//                   src={item.image}
+//                   alt=""
+//                 />
+//               )}
+//             </div>
+//           ))}
+//           <button onClick={nextClick}>
+//             <TfiAngleRight />
+//           </button>
+//         </div>
+
+//         <div className="landing-right">
+//           <h1 className="right-one">
+//             We have certified dealerships from a number of companies
+//           </h1>
+//           <h1 className="right-two">
+//             Explore more of our products from the{' '}
+//             <Link to={'/products'}>Products</Link> section{' '}
+//           </h1>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
 const LandingPage = () => {
-  const carousel = [
-    {
-      id: 1,
-      image: m1,
-    },
-    {
-      id: 2,
-      image: m2,
-    },
-    {
-      id: 3,
-      image: m3,
-    },
-  ]
-
-  console.log(carousel)
-
-  const length = carousel.length
-  const [click, setClick] = useState(0)
-
-  const prevClick = () => {
-    setClick(click === length - 1 ? 0 : click + 1)
-  }
-
-  const nextClick = () => {
-    setClick(click === 0 ? length - 1 : click - 1)
-  }
-
-  const [num, setNum] = useState(0)
-  useEffect(() => {
-    if (num < 250) {
-      setNum((num) => num + 1)
-    } else {
-      setNum(num)
-    }
-  })
-
   return (
-    <div className="landing-page">
-      <div className="landing-top">
-        <div className="intro">
-          <span className="s1">
-            Experience the best services from the most trusted
-          </span>
-          <span className="s2">JP Cut & Weld</span>
-        </div>
-        <div className="nums">
-          <div className="num1 num">
-            <p>Our count of happy customers is increasing everyday!</p>
-            <h1>{num}+</h1>
-          </div>
-          <div className="num2 num">
-            <p>Number of places we have provided services...</p>
-            <h1>{num}+</h1>
-          </div>
-          <div className="num3 num">
-            <p>
-              We strive to be better than yesterday and give the best than we
-              gave yesterday.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="landing-middle">
-        <div className="landing-left">
-          <button onClick={prevClick}>
-            <TfiAngleLeft />
-          </button>
-          {carousel.map((item, index) => (
-            <div
-              className={index === click ? 'carousel active' : 'carousel'}
-              key={index}
-            >
-              {index === click && (
-                <img
-                  className="car-image"
-                  src={item.image}
-                  alt=""
-                />
-              )}
-            </div>
-          ))}
-          <button onClick={nextClick}>
-            <TfiAngleRight />
-          </button>
-        </div>
-
-        <div className="landing-right">
-          <h1 className="right-one">
-            We have certified dealerships from a number of companies
-          </h1>
-          <h1 className="right-two">
-            Explore more of our products from the{' '}
-            <Link to={'/products'}>Products</Link> section{' '}
-          </h1>
-        </div>
-      </div>
+    <div className="landing-container">
+      <h1>hello</h1>
     </div>
   )
 }
